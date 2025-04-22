b8fb5aa19c4ef0d46d16477051e922bfdc7646cd
keen1014 <103188823+keen1014@users.noreply.github.com>
2025-04-22 10:02:01 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,12 @@
+x=int(input())
+y=int(input())
+if 1<x:
+    if 1 < y:
+        print(1)
+    elif y<1:
+        print(4)
+elif x<1:
+    if 1 < y:
+        print(2)
+    elif y<1:
+        print(3)
