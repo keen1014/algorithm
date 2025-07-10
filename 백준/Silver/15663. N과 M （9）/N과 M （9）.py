016faaf36e093e8b75098d00e6e064922c053de0
keen1014 <103188823+keen1014@users.noreply.github.com>
2025-07-10 21:49:22 +0900

    [Silver II] Title: N과 M (9), Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,21 @@
+def nm(N):
+    if N==m:
+        print(*li)
+        return
+    remember_num=0
+    for i in range(n):
+        if not vis[i] and remember_num!=stack[i]:
+            vis[i]=True
+            li[N]=stack[i]
+            remember_num=stack[i]
+            nm(N+1)
+            vis[i]=False
+            
+                
+import sys
+input=sys.stdin.readline
+n, m=map(int, input().strip().split())
+stack=sorted(list(map(int, input().strip().split())))
+vis=[False]*n
+li=[0]*m
+nm(0)
