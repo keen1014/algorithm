61a17da610e63a72bac08768776b92398f27bda9
keen1014 <103188823+keen1014@users.noreply.github.com>
2025-07-10 10:52:59 +0900

    [Silver III] Title: N과 M (7), Time: 2012 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,15 @@
+def nm(N):
+    if N==m:
+        print(*li)
+        return
+    for i in range(n):
+        if not vis[i]:
+            li[N]=stack[i]
+            nm(N+1)
+import sys
+input=sys.stdin.readline
+n, m=map(int, input().strip().split())
+stack=sorted(list(map(int, input().strip().split())))
+vis=[False]*n
+li=[0]*m
+nm(0)
