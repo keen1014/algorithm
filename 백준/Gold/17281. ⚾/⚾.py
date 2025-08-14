16161bb4d88b88c70ef752fe0112cf85dc630073
keen1014 <103188823+keen1014@users.noreply.github.com>
2025-08-15 00:30:40 +0900

    [Gold IV] Title: ⚾, Time: 1444 ms, Memory: 208820 KB -BaekjoonHub

---
+++ 백준/Gold/17281. ⚾/⚾.py
@@ -0,0 +1,58 @@
+def playininning():
+    now=0
+    ans=0
+    for z in range(N):
+        ru1=ru2=ru3=out=0
+        while out!=3:
+            tmp=li[now]
+            ins=inning[z][tmp]
+            
+            if ins==0:
+                out+=1
+            elif ins== 1:
+                ans+=ru3
+                ru1, ru2, ru3= 1, ru1, ru2
+                
+            elif ins== 2:
+                ans+=(ru3+ru2)
+                ru1, ru2, ru3= 0, 1, ru1
+            elif ins== 3:
+                ans+=(ru3+ru2+ru1)
+                ru1, ru2, ru3= 0, 0, 1
+            elif ins== 4:
+                ans+=(ru3+ru2+ru1+1)
+                ru1, ru2, ru3= 0, 0, 0
+            now+=1
+            if 9<=now:
+                now=0
+
+        answer.append(ans)
+        
+def backtracking(n):
+    if n == 9:
+        # print(li)
+        playininning()
+        return
+    for i in range(9):
+        if i==0 or vis[i]==True:
+            continue
+        vis[i]=True
+        if 3==n:
+            li[n+1]=i
+            backtracking(n+2)
+        else:
+            li[n]=i
+            backtracking(n+1)
+        vis[i]=False
+        
+import sys
+from collections import deque
+input=sys.stdin.readline
+N=int(input().strip())
+li=[0]*9
+vis=[False]*9
+answer=[]
+inning=[list(map(int, input().strip().split())) for i in range(N)]
+# print(*inning, sep='\n')
+backtracking(0)
+print(max(answer))
