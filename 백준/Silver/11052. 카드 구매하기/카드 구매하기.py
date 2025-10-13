17b9d5910c3ae11f78397d01b4480e209924c9fe
keen1014 <103188823+keen1014@users.noreply.github.com>
2025-10-13 10:17:44 +0900

    [Silver I] Title: 카드 구매하기, Time: 172 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,10 @@
+import sys
+input=sys.stdin.readline
+N=int(input().strip())
+p=list(map(int, input().strip().split()))
+dp=[0 for i in range(N+1)]
+    
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        dp[i]=max(dp[i],dp[i-j]+p[j-1])
+print(dp[N])
