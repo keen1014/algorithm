288d35a2f3bcbade8dc9ec30114381ba345fda3e
keen1014 <103188823+keen1014@users.noreply.github.com>
2025-10-27 23:00:10 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum-10-27-2025-23-00-09.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findPrimePairs(self, n: int) -> List[List[int]]:
+        dp=[1 for i in range(1000001)]
+        dp[0]=0
+        dp[1]=0
+        for i in range(2, (n//2)+1):
+            if dp[i]:
+                for j in range(i * i, n+1, i):
+                    dp[j]=0
+        answer=[]
+        for i in range(2,(n//2)+1):
+            if dp[i] and dp[n-i]:
+                answer.append([i,n-i])
+        return answer
